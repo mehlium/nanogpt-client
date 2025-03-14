f532e58a975e4ede75e002659938963baa835554
Kjetil Mehl <kjetil.mehl@husbanken.no>
2025-03-14 14:13:59 +0100

    Fixes

---
+++ src/nanogptclient.ts
@@ -0,0 +1,25 @@
+import { createChatCompletion, Options } from './sdk.gen.js'
+import { CreateChatCompletionData } from './types.gen.js'
+
+type APIKey = string
+
+export class NanoGPTClient {
+  apiKey: string
+
+  constructor(apiKey: APIKey) {
+    this.apiKey = apiKey
+  }
+
+  chat<ThrowOnError extends boolean = false>(
+    options: Options<CreateChatCompletionData, ThrowOnError>
+  ) {
+    return createChatCompletion({
+      ...options,
+      headers: { Authorization: `Bearer: ${this.apiKey}` }
+    })
+  }
+}
+
+export const foo = () => {
+  console.log('void')
+}
